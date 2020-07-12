4a7793e8f8d88d67de218727211b7b0a0e035eb2
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-07-13 04:35:04 +0900

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 server.listen(port, function () {
-    console.log(`Сервер запущен на ${port} порту\n`);
+    console.log(`Сервер запущен на ${port} порту\nРежим: ${process.env.MODE}\n`);
 });
 
 /*
