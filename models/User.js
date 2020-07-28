4958e1fded734b7e55292dedcec1e20dd7f0aeab
gwynbleidd10 <gwynbleidd10@gmail.com>
2020-07-28 21:31:30 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const { Schema, model } = require('mongoose')
 const schema = new Schema({
     super: { type: Boolean, default: false },
     tg: { type: String, unique: true },
-    name: { type: String, default: null },
+    name: { type: String, default: '' },
     dept: { type: String, default: null }
 }, { versionKey: false })
 
-module.exports = model('User', schema)
+module.exports = model('users', schema)
